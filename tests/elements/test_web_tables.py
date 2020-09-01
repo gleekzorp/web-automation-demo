60f9e7e13f4896d6737b1056cbbf7e22bcb23ab8
Daniel Floyd <gleek.dan@gmail.com>
2020-09-01 16:53:16 -0600

    first challenge for web tables added

--- tests/elements/test_web_tables.py
+++ tests/elements/test_web_tables.py
@@ -4,3 +4,18 @@ import pytest
 @pytest.fixture
 def demo_qa(py):
     py.visit('https://demoqa.com/webtables')
+
+
+def test_add_user(py, demo_qa):
+    py.get('#addNewRecordButton').click()
+    py.get('#firstName').type('Daniel')
+    py.get('#lastName').type('Floyd')
+    py.get('#userEmail').type(py.fake.email())
+    py.get('#age').type('20')
+    py.get('#salary').type('100000')
+    py.get('#department').type(py.fake.job())
+    py.get('#submit').click()
+    assert py.get('.rt-tbody').should().contain_text('Daniel')
+
+
+# py.wait(use_py=True).sleep(5)
